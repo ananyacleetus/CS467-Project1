c0c0c1be139a4af4877ee88dba4b80b1809f4841
ananyacleetus <ananyacleetus@gmail.com>
2021-02-20 17:07:26 -0500

    added starter code

--- README.md
+++
@@ -1,3 +0,0 @@
-# CS467-Project 1: Stock Market Manipulation through Social Media
-
-Repo for CS467 Project 1
---
+++ src/index.js
@@ -0,0 +1,37 @@
+/**
+ * Main entry point -- this file has been added to index.html in a <script> tag. Add whatever imports and code you
+ * want below.  For a detailed explanation of JS modules, see
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+ */
+import * as d3 from "d3";
+
+const data = [
+    {h: 1, color: "red"},
+    {h: 2, color: "green"},
+    {h: 3, color: "purple"},
+    {h: 4, color: "orange"},
+    {h: 5, color: "blue"},
+];
+
+/*
+ * Why this line? Because if this script runs before the svg exists, then nothing will happen, and d3 won't even
+ * complain.  This delays drawing until the entire DOM has loaded.
+ */
+window.addEventListener("load", drawBars);
+
+
+var y = d3.scaleLinear()
+  .domain([6, 0])
+  .range([100, 0]);
+
+function drawBars() {
+    const svg = d3.select("svg");
+    svg.selectAll("rect")
+        .data(data)
+        .join("rect")
+            .attr('x', function(d, i) { return 20 + (25 * i)} )
+            .attr("y", function(d) { return 100 - y(d.h); } )
+            .attr("height", function(d) { return y(d.h); } )
+            .attr("width", 20)
+            .attr("fill", d => d.color);
+}
