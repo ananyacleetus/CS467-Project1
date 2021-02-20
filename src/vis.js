493b741c32be90a81a6efb581220aa61772b781b
ananyacleetus <ananyacleetus@gmail.com>
2021-02-20 18:26:17 -0500

    starting graph

--- src/vis.js
+++ src/vis.js
@@ -1,32 +1,70 @@
 import * as d3 from "d3";
 
-const data = [
-    {h: 1, color: "red"},
-    {h: 2, color: "green"},
-    {h: 3, color: "purple"},
-    {h: 4, color: "orange"},
-    {h: 5, color: "blue"},
-];
+// const data = [
+//     {h: 1, color: "red"},
+//     {h: 2, color: "green"},
+//     {h: 3, color: "purple"},
+//     {h: 4, color: "orange"},
+//     {h: 5, color: "blue"},
+// ];
+
+
 
 /*
  * Why this line? Because if this script runs before the svg exists, then nothing will happen, and d3 won't even
  * complain.  This delays drawing until the entire DOM has loaded.
  */
-window.addEventListener("load", drawBars);
+window.addEventListener("load", drawLineGraph);
+
+var MARGIN = {TOP: 30, RIGHT: 50, BOTTOM: 30, LEFT: 50}
+
+function drawLineGraph() {
+
+  const svg = d3.select("svg");
+
+  const data = d3.csv("/fakedata.csv").then(function(data) {
+
+  //Should return month-day-year
+  const dateFormat = d3.timeParse("%d-%b-%y");
+
+  //Should return hourv(12h format) : minute : am/pm
+  const timeFormat = d3.timeParse("%I:%M %p");
+
+  const minDate = d3.min(data, function(d) { return dateFormat(d.date); });
+  const maxDate = d3.max(data, function(d) { return dateFormat(d.date); });
+  const minPrice = d3.min(data, function(d) { return d.price; });
+  const maxPrice = d3.max(data, function(d) { return d.price; });
+
+  //Get the current height and width of the SVG
+  const svgwidth = svg.attr("width");
+  const svgheight = svg.attr("height");
+
+  var dateScale = d3.scaleTime()
+  .domain(minDate, maxDate)
+  .range([0, svgwidth]);
+
+  var priceScale = d3.scaleLinear()
+  .domain(0, maxPrice)
+  .range([svgheight, 0]);
+
+  var currentline = d3.line()
+  .x(function(d) {
+    console.log(d.date);
+    console.log(dateFormat(d.date));
+    console.log(dateScale(dateFormat(d.date)));
+
+    return dateScale(dateFormat(d.date)); })
+
+  .y(function(d) { return priceScale(d.price); });
+
+  svg.append("path")
+    .data([data])
+    .attr("d", currentline)
+    .attr("class", "chartLine");
+
+});
 
 
-var y = d3.scaleLinear()
-  .domain([6, 0])
-  .range([100, 0]);
 
-function drawBars() {
-    const svg = d3.select("svg");
-    svg.selectAll("rect")
-        .data(data)
-        .join("rect")
-            .attr('x', function(d, i) { return 20 + (25 * i)} )
-            .attr("y", function(d) { return 100 - y(d.h); } )
-            .attr("height", function(d) { return y(d.h); } )
-            .attr("width", 20)
-            .attr("fill", d => d.color);
+
 }
