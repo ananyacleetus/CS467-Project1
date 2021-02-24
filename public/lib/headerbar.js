fee202b80a4a641b4cf95c6197b4188c11bdcf4a
ananyacleetus <ananyacleetus@gmail.com>
2021-02-24 02:25:41 -0500

    fixing headeR

--- public/lib/headerbar.js
+++ public/lib/headerbar.js
@@ -17,27 +17,4 @@ class HeaderBar extends React.Component {
 }
 
 var domContainer = document.querySelector('#headerbar');
-ReactDOM.render( /*#__PURE__*/React.createElement(HeaderBar, null), domContainer); //
-// 'use strict';
-//
-// class LikeButton extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { liked: false };
-//   }
-//
-//   render() {
-//     if (this.state.liked) {
-//       return 'You liked this.';
-//     }
-//
-//     return (
-//       <button onClick={() => this.setState({ liked: true }) }>
-//         Like
-//       </button>
-//     );
-//   }
-// }
-//
-// let domContainer = document.querySelector('#like_button_container');
-// ReactDOM.render(<LikeButton />, domContainer);
+ReactDOM.render( /*#__PURE__*/React.createElement(HeaderBar, null), domContainer);
