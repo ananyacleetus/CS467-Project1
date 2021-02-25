84fd0094c558514d2365d4d7458589be1322cbfd
ananyacleetus <ananyacleetus@gmail.com>
2021-02-25 10:43:10 -0500

    fixed chart useeffect

--- public/lib/scalebar.js
+++ public/lib/scalebar.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab'; //stylesheet
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+import { Icon } from '@material-ui/core';
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons'; //stylesheet
 
 import "..//css/scalebar.css";
 
